20c70c2d1fccf6e16add414c42fe79e4724144d6
luc <luc.willems@it4y.eu>
2015-02-04 19:57:48 +0100

    test

---
+++ test_arduinp/test_arduinp.h
@@ -0,0 +1,29 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _test_arduinp_H_
+#define _test_arduinp_H_
+#include "Arduino.h"
+//add your includes for the project test_arduinp here
+
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project test_arduinp here
+
+
+
+
+//Do not add code below this line
+#endif /* _test_arduinp_H_ */
